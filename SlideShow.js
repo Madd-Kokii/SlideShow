aaa11739737a2f8eba34d9afb9305604b1d3f7d0
Madd-Kokii <167290736+Madd-Kokii@users.noreply.github.com>
2024-05-15 16:32:59 +0200

    Add files via upload

---
+++ SlideShow.js
@@ -0,0 +1,19 @@
+let Indexslide = 0;
+showSlideshow();
+
+function showSlideshow() {
+    let slides = document.getElementsByClassName("slider-show");
+
+    for( i= 0 ;i < slides.length; i++ ) {
+        slides[i].style.display = "none";
+        slides[i].classList.remove("slider")
+    }
+    Indexslide ++;
+
+    if(Indexslide > slides.length){
+        Indexslide = 1
+    }
+        slides[Indexslide - 1].style.display = "block";
+        slides[Indexslide - 1].classList.add("slider")
+        setTimeout(showSlideshow, 10000);
+}
